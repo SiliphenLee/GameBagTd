de42c9434f85663bc13dbd9678a164747c76d14f
Siliphen <scorpion2007@qq.com>
2024-11-17 17:36:08 +0800

    11

--- cocos-js/box2d.release.wasm-24014bf2.js
+++ cocos-js/box2d.release.wasm-24014bf2.js
@@ -1,1 +1,11 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/box2d.release.wasm-4b534afe.wasm")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var box2d_release_wasm = exports('default', 'assets/box2d.release.wasm-4b534afe.wasm'); /* asset-hash:560834ef */
+
+		})
+	};
+}));
+//# sourceMappingURL=box2d.release.wasm-24014bf2.js.map
--- cocos-js/spine.js-6770127a.js
+++ cocos-js/spine.js-6770127a.js
@@ -1,1 +1,11 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-dc937d48.bin")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine_js = exports('default', 'assets/spine.js.mem-dc937d48.bin'); /* asset-hash:74cbe1d2 */
+
+		})
+	};
+}));
+//# sourceMappingURL=spine.js-6770127a.js.map
